6732a1f8a6abbff12383ecd3729607ad8f1f4339
gandhiparth16 <69316051+gandhiparth16@users.noreply.github.com>
2021-01-03 11:29:02 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ul id="1">Red</ul>
+      <ul id="2">Green</ul>
+      <ul id="3">Blue</ul>
+      <ul id="3">Blue</ul>
+      <ul id="3">Blue</ul>
+      <ul id="3">Blue</ul>
+      <ul id="3">Blue</ul>
+      <ul id="3">Blue</ul>
+      <ul id="3">Blue</ul>
+      <ul id="3">Blue</ul>
+      <ul id="3">Blue</ul>
+    </div>
+  );
+}
